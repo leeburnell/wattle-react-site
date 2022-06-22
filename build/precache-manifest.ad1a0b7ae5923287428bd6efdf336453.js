494337fe9f783e6f23c3fb681435058ced12b775
Lee Burnell <iambeeroy@gmail.com>
2022-06-22 21:22:26 +0100

    first commit

---
+++ build/precache-manifest.ad1a0b7ae5923287428bd6efdf336453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "40efed3dda1a0ce67d84",
+    "url": "/demo/react/abstrak/static/css/main.b56e5e3a.chunk.css"
+  },
+  {
+    "revision": "40efed3dda1a0ce67d84",
+    "url": "/demo/react/abstrak/static/js/main.777568b0.chunk.js"
+  },
+  {
+    "revision": "bbebb96e258665594920",
+    "url": "/demo/react/abstrak/static/js/runtime~main.9c3924fd.js"
+  },
+  {
+    "revision": "f8c04ed6ce2b01175514",
+    "url": "/demo/react/abstrak/static/css/2.206b709b.chunk.css"
+  },
+  {
+    "revision": "f8c04ed6ce2b01175514",
+    "url": "/demo/react/abstrak/static/js/2.ef378ac8.chunk.js"
+  },
+  {
+    "revision": "d03d05430b9a42cc73e3",
+    "url": "/demo/react/abstrak/static/js/3.06a4cbf0.chunk.js"
+  },
+  {
+    "revision": "626445251ce5bc8925c98dad32a1ade1",
+    "url": "/demo/react/abstrak/index.html"
+  }
+];
